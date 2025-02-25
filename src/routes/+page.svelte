52fda3b8c8aa1d4fd419cfb11425f6cd8926bb56
skutsch <skutsch@iastate.edu>
2025-02-25 12:16:05 -0600

    tutorial completed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,15 @@
     let selected = [];
     let matches = [];
 
+    let timerID = null;
+    let time = 60;
+
+    function startGameTimer() {
+        function countDown(){
+            currentState != 'paused' && (time -= 1);
+        }
+        timerID = setInterval(countDown, 1000);
+    }
 
     function createGrid() {
         let cards = new Set();
@@ -37,21 +46,62 @@
         const [firstCard, secondCard] = selected;
 
         if(grid[firstCard] == grid[secondCard]){
-            matches == matches.concat(grid[first]);
+            matches = matches.concat(grid[firstCard]);
         }
         //Clear after 300 ms when 2 cards are selected
-        setTimeout(() => selected=[], 300); 
+        setTimeout(() => selected = [], 300); 
+    }
+
+    function resetGame() {
+        timerID && clearInterval(timerID);
+        grid = createGrid();
+        maxMatches = grid.length / 2;
+        selected = [];
+        matches = [];
+        timerID = null;
+        time = 20;
     }
 
     function gameWon() {
         currentState = 'won';
+        resetGame();
+    }
+
+    function gameLost() {
+        currentState = 'lost';
+        resetGame();
+    }
+
+    function pauseGame(e) {
+        if(e.key === 'Escape'){
+            switch(currentState){
+                case 'playing': 
+                    currentState = 'paused';
+                    break;
+                case 'paused':
+                    currentState = 'playing';
+                    break;
+            }
+        }
+    }
+
+    $: if (currentState === 'playing'){
+        !timerID && startGameTimer();
     }
 
     $: selected.length === 2 && matchCards();
     $: maxMatches === matches.length && gameWon();
-    $: console.log({State, selected, matches});
+    $: time === 0 && gameLost();
+
+    $: console.log(currentState, selected, matches);
 
 </script>
+
+<svelte:window on:keydown={pauseGame}></svelte:window>
+
+{#if currentState === 'paused'}
+    <h1>Game Paused</h1>
+{/if}
 
 {#if currentState === 'start'}
     <h1>Matching Game</h1>
@@ -59,6 +109,8 @@
 {/if}
 
 {#if currentState === 'playing'}
+<h1 class="timer" class:pulse={ time<=10 }>{time}</h1>
+
     <div class="matches">
         {#each matches as card}
             <div>{card}</div>    
@@ -67,14 +119,17 @@
     
     <div class="cards">
         {#each grid as card, cardIndex}
+            <!--Constants-->
             {@const isSelected = selected.includes(cardIndex)}
             {@const isSelectedOrMatched = selected.includes(cardIndex) || matches.includes(card)}
             {@const match = matches.includes(card)}
+
             <button class="card" 
+                    class:flip={isSelectedOrMatched}
                     class:selectedCard={isSelected} 
                     disabled={isSelectedOrMatched}
                     on:click={() => selectCard(cardIndex)}>
-                <div class:match>{card}</div>
+                <div class="back" class:match>{card}</div>
             </button>
         {/each}
     </div>
@@ -103,12 +158,28 @@
         width: 140px;
         font-size: 4rem;
         background-color: var(--bg-2);
-     
+        transition: rotate 0.3s ease-out;
+        transform-style: preserve-3d;
+
         &.selectedCard {
             border: 4px solid var(--border);    
         }
 
-        .match {
+        &.flip {
+            rotate: y 180deg;
+            pointer-events: none;
+        }
+
+        & .back {
+            position: absolute;
+            inset: 0;
+            display: grid;
+            place-content: center;
+            backface-visibility: hidden;
+            rotate: y 180deg;
+        }
+
+        & .match {
             transition: opacity 0.3s ease-out;
             opacity: 0.4;
         }
@@ -119,6 +190,21 @@
         gap: 1rem;
         margin-block: 2rem;
         font-size: 3rem;
+    }
+
+    .timer {
+        transition: color 0.3s ease;
+    }
+
+    .pulse {
+        color: var(--pulse);
+        animation: pulse 1s infinite ease;
+    }
+
+    @keyframes pulse {
+        to {
+            scale: 1.4;
+        }
     }
 
 </style>
